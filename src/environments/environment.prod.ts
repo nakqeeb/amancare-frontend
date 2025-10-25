77612b22ba89306e9373b6dd6690f784dce1e360
Khalid Al-Yafey <59516691+nakqeeb@users.noreply.github.com>
2025-10-25 06:06:36 +0300

    Revert "prod apiUrl"
    This reverts commit be4e03f5da5ee8f5888bdcd381d6fd890ef47373.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
-  // apiUrl: 'https://api.amancare.com/v1',
-  apiUrl: 'http://localhost:8080/api/v1',
+  apiUrl: 'https://api.amancare.com/v1',
   appName: 'نظام أمان كير',
   version: '1.0.0',
   defaultLanguage: 'ar',
