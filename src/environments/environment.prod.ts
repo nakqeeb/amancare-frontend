be4e03f5da5ee8f5888bdcd381d6fd890ef47373
Khalid Al-Yafey <59516691+nakqeeb@users.noreply.github.com>
2025-10-25 06:01:40 +0300

    prod apiUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.amancare.com/v1',
+  // apiUrl: 'https://api.amancare.com/v1',
+  apiUrl: 'http://localhost:8080/api/v1',
   appName: 'نظام أمان كير',
   version: '1.0.0',
   defaultLanguage: 'ar',
